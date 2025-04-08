2c65c6a87fee7e340d5c503292cc896727addaf1
yacinekahlis <yacine.kahlis.1@ulaval.ca>
2025-04-08 17:34:12 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { Toaster } from "sonner"
 import "./globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <html lang="fr">
         <body className={inter.className}>
         {children}
+        <Analytics/>
         <Toaster position="bottom-right" />
         </body>
         </html>
